c8643b52f067cfb62c8463243eccfbcdec2bc5cf
Mike Andrzejewski <mike.aski@gmail.com>
2015-10-07 12:16:35 +0200

    Ensure failing QUnit tests fails build

--- tasks/test/unit/run.js
+++ tasks/test/unit/run.js
@@ -2,11 +2,11 @@
 var qunit = require('node-qunit-phantomjs');
 
 module.exports = function (gulp) {
-  return function () {
+  return function (callback) {
     return qunit('./dist/tests/unit-cli.html', {
       'phantomjs-options': ['--ssl-protocol=any'],
       'timeout': 5,
       'verbose': true
-    });
+    }, callback);
   };
 };
